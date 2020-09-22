c6e9db9d200456a6bc9b0fe9fcf7f060bd0bbcb9
lucaspuglielli <lucaspuglielli@gmail.com>
2020-09-21 22:05:56 -0300

    adicão da base em node

---
+++ routes/index.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Phoenix' });
+});
+
+router.get('/azeroth', function(req, res, next) {
+  res.render('azeroth', { title: 'Azeroth Heroes' });
+});
+
+router.get('/contato', function(req, res, next) {
+  res.render('contato', { title: 'Contato' });
+});
+
+router.get('/death', function(req, res, next) {
+  res.render('death', { title: 'Death and Darkness' });
+});
+
+router.get('/encontro', function(req, res, next) {
+  res.render('encontro', { title: 'Encontro da Phoenix' });
+});
+
+router.get('/equipes', function(req, res, next) {
+  res.render('equipes', { title: 'Equipes' });
+});
+
+router.get('/eventos', function(req, res, next) {
+  res.render('eventos', { title: 'Eventos' });
+});
+
+router.get('/frontline', function(req, res, next) {
+  res.render('frontline', { title: 'Frontline' });
+});
+
+router.get('/gogoboys', function(req, res, next) {
+  res.render('gogoboys', { title: 'Gogoboys' });
+});
+
+router.get('/guildrun', function(req, res, next) {
+  res.render('guildrun', { title: 'Guild Run' });
+});
+
+router.get('/loja', function(req, res, next) {
+  res.render('loja', { title: 'Loja' });
+});
+
+router.get('/loktar', function(req, res, next) {
+  res.render('loktar', { title: 'Loktar Ogar' });
+});
+
+router.get('/quem-somos', function(req, res, next) {
+  res.render('quem-somos', { title: 'Quem Somos' });
+});
+
+router.get('/sindicato', function(req, res, next) {
+  res.render('sindicato', { title: 'Sindicato do Crime' });
+});
+
+router.get('/template', function(req, res, next) {
+  res.render('template', { title: 'Template' });
+});
+
+router.get('/transmissoes', function(req, res, next) {
+  res.render('transmissoes', { title: 'Transmissões' });
+});
+
+
+module.exports = router;
